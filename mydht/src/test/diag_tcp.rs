68307003584fc9c9ad2f0d1db732615a7dfd973d
cheme <emericchevalier.pro@gmail.com>
2018-03-08 20:56:22 +0100

    Test nat traversal

--- mydht/src/test/diag_tcp.rs
+++ mydht/src/test/diag_tcp.rs
@@ -3,20 +3,48 @@
 //!
 
 extern crate mydht_tcp;
+extern crate igd;
+
+#[cfg(not(windows))]
+extern crate libc;
+
+#[cfg(not(windows))]
+extern crate ipnetwork;
+#[cfg(not(windows))]
+use self::libc::{
+  getifaddrs,
+  freeifaddrs,
+  ifaddrs,
+  sockaddr,
+  sockaddr_in,
+  AF_INET,
+};
+
+#[cfg(not(windows))]
+use self::ipnetwork::{
+  ip_mask_to_prefix,
+  Ipv4Network,
+};
+
+#[cfg(not(windows))]
+use std::net::{IpAddr};
+
 use procs::api::{
   DHTIn,
 };
-
+use std::mem;
+use std::ptr;
 use msgenc::json::Json;
 use self::mydht_tcp::Tcp;
 use utils;
 use transport::{
   SerSocketAddr,
 };
-use std::net::{Ipv4Addr};
+use std::net::{SocketAddrV4,Ipv4Addr};
 #[cfg(test)]
 use mydht_basetest::transport::connect_rw_with_optional;
 use time::Duration;
+use std::time::Duration as StdDuration;
 use super::{
   initpeers2,
   DHTRULES_DEFAULT,
@@ -68,6 +96,79 @@ fn connect_rw () {
 
   connect_rw_with_optional(tcp_transport_1,tcp_transport_2,&a1,&a2,true,false);
 }
+
+
+#[cfg(windows)]
+fn ip_addr_for_gateway (_gateway : &Ipv4Addr) -> Option<Ipv4Addr> {
+  None
+}
+// dirty select (should use something from a designated crate)
+#[cfg(not(windows))]
+fn ip_addr_for_gateway (gateway : &Ipv4Addr) -> Option<Ipv4Addr> {
+  let mut matches = 0;
+  let mut res = None;
+
+  let mut addresses: *mut ifaddrs = unsafe { mem::zeroed() };
+  if unsafe { getifaddrs(&mut addresses as *mut _) } != 0 {
+    return None;
+	}
+  let mut p_address: *mut ifaddrs = addresses;
+  while p_address != ptr::null_mut() {
+    let add = unsafe { *(*p_address).ifa_addr };
+    if add.sa_family as i32 == AF_INET {
+      let mask = unsafe { *(*p_address).ifa_netmask }.sa_data;
+      let prefix = ip_mask_to_prefix(IpAddr::V4(Ipv4Addr::new(
+            mask[2] as u8,
+            mask[3] as u8,
+            mask[4] as u8,
+            mask[5] as u8,
+      ))).unwrap();
+      let add = add.sa_data;
+      // may want to cast to sock_addr_in for possible different layout
+      let address = Ipv4Addr::new(
+            add[2] as u8,
+            add[3] as u8,
+            add[4] as u8,
+            add[5] as u8,
+      );
+      if Ipv4Network::new(gateway.clone(),prefix).unwrap().network() == 
+         Ipv4Network::new(address.clone(),prefix).unwrap().network() {
+           res = Some(address);
+           break;
+      }
+    }
+  	p_address = unsafe { (*p_address).ifa_next };
+	}
+	unsafe { freeifaddrs(addresses) };
+  res
+}
+#[test]
+fn connect_rw_upnp () {
+  let start_port = 50000;
+
+//  let local_ip = Ipv4Addr::new(127,0,0,1);
+  let mut local_ip = Ipv4Addr::new(0,0,0,0);
+  // local v4 ip only (gateway from igd is v4)
+
+  let a1 = SerSocketAddr(utils::sa4(local_ip.clone(), start_port));
+  let a2 = SerSocketAddr(utils::sa4(local_ip.clone(), start_port+1));
+  let gateway = igd::search_gateway_timeout(StdDuration::from_secs(5)).unwrap();
+  let pub_ip = gateway.get_external_ip().unwrap();
+  if let Some(a) = ip_addr_for_gateway(gateway.addr.ip()) {
+    local_ip = a;
+  }
+  let pub_port1 = gateway.add_any_port(igd::PortMappingProtocol::TCP, SocketAddrV4::new(local_ip.clone(), start_port), 0, "test tcp upnp add1").unwrap();
+  let pub_port2 = gateway.add_any_port(igd::PortMappingProtocol::TCP, SocketAddrV4::new(local_ip.clone(), start_port+1), 0, "test tcp upnp add2").unwrap();
+
+  let d1 = SerSocketAddr(utils::sa4(pub_ip.clone(), pub_port1));
+  let d2 = SerSocketAddr(utils::sa4(pub_ip.clone(), pub_port2));
+  let tcp_transport_1 : Tcp = Tcp::new (&a1, Duration::seconds(5), true).unwrap();
+  let tcp_transport_2 : Tcp = Tcp::new (&a2, Duration::seconds(5), true).unwrap();
+
+  connect_rw_with_optional(tcp_transport_1,tcp_transport_2,&d1,&d2,true,false);
+}
+
+
 
 #[test]
 fn connect_rw_dup () {
